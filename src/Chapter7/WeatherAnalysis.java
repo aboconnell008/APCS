a9fa7aa347bf9cb8fbaca40d0004d829851edb08
AbbyOConnell <aboconnell@student.mamkschools.org>
2018-10-17 14:22:25 -0400

    Commit

--- src/Chapter7/WeatherAnalysis.java
+++ src/Chapter7/WeatherAnalysis.java
@@ -19,7 +19,7 @@ public class WeatherAnalysis {
         int[] numbers = new int[a];
         // Step 3 : add the numbers
         int sum=0;
-        //Forloop for adding numbers to the array
+        //adds numbers to the array
         for (int i = 1; i<=a; i++){
             System.out.println("Enter Data:");
             int x =input.nextInt();
@@ -31,38 +31,11 @@ public class WeatherAnalysis {
         //gets the average
         double avg = (double)sum/a;
         avg = (Math.round(avg * 10.0)) / 10.0;
-        System.out.println("Average temp = " + avg);
-        above(avg, numbers, a);
-        smaller(avg, numbers, a);
+        System.out.println("The Average tempurature was " + avg);
 
-
-
-    }
-    //what is above average
-    public static void above(double avg, int[] array, int n) {
-        int larger = 0;
-        for (int i = 1; i <= n; i++) {
-            if (array[i - 1] > avg) {
-                larger++;
-            }
-        }
-
-            System.out.println(larger + " days were above average.");
 
 
 
     }
-    public static void smaller(double avg, int[] array, int n) {
-        int smaller = 0;
-        for (int i = 1; i <= n; i++) {
-            if (array[i - 1] > avg) {
-                smaller++;
-            }
-        }
 
-            System.out.println(smaller + " days were above average.");
-
-
-
-    }
 }
